0ab461237fb55162530cb311248a3d68e71a347f
Brian Goncalves <briancollog@hotmail.com>
2019-01-26 12:25:58 -0200

    musics

--- GGJ2019/Assets/Scripts/Box/BoxMovement.cs
+++ GGJ2019/Assets/Scripts/Box/BoxMovement.cs
@@ -9,36 +9,44 @@ public class BoxMovement : MonoBehaviour {
     public bool IsMoving = false;
     public Vector3 MoveSide;
 
-    Transform trans;
-
     private void Start()
     {
-        trans = GetComponent<Transform>();
+        targetPosition = transform.position;
     }
 
-    private void FixedUpdate()
+    public Vector3 targetPosition;
+    //public Quaternion targetRotation; //Optional of course
+    public float smoothFactor = 2;
+    void Update()
     {
-        // when moving is set, translate to the set direction
-        if (IsMoving)
-        {
-            trans.Translate(MoveSide * Time.deltaTime * MoveSpeed);
-        }
+        transform.position = Vector3.Lerp(transform.position, targetPosition, Time.deltaTime * smoothFactor);
+        //transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime * smoothFactor);
     }
 
     public void MoveBox(Vector3 Side)
     {
         // Set the move direction, called by the player
-        IsMoving = true;
-        MoveSide = Side;
-    }
-
-    private void OnCollisionEnter(Collision collision)
-    {
-        // if is moving and collide, it stop moving
-        if (IsMoving)
+        //IsMoving = true;
+        //MoveSide = Side;
+        Vector3 myPosition = transform.position;
+        Vector3 rayDirection = Side;
+        //float rayLengthMeters = WallDistance;
+        RaycastHit hitInfo;
+        
+        if (Physics.Raycast(myPosition, rayDirection, out hitInfo))
         {
-            IsMoving = false;
-            trans.Translate(Vector3.zero);
+            Debug.DrawLine(myPosition, hitInfo.collider.gameObject.transform.position);
+            targetPosition = hitInfo.collider.gameObject.transform.position;
         }
     }
+
+    //private void OnCollisionEnter(Collision collision)
+    //{
+    //    // if is moving and collide, it stop moving
+    //    if (IsMoving)
+    //    {
+    //        IsMoving = false;
+    //        trans.Translate(Vector3.zero);
+    //    }
+    //}
 }
