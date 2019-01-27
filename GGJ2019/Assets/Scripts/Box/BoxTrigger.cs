767a59d76c7d568f92413978e4628463e5a5fc9b
Camilla Camatta <camillacamatta@gmail.com>
2019-01-27 01:38:12 -0200

    more

---
+++ GGJ2019/Assets/Scripts/Box/BoxTrigger.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxTrigger : MonoBehaviour {
+    public BoxMovement box;
+    public Direction direct;
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Trigger");
+        if (other.gameObject.layer.Equals(LayerMask.NameToLayer("Player")))
+        {
+            box.direct = direct;
+        }
+    }
+}
+
+public enum Direction { Left, Right, Back, Forward }
